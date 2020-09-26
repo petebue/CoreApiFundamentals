e4637f255871ce1c0e942841b69418a11d3258d0
pbuelow <buelow.peter@gmail.com>
2020-09-26 14:36:09 -0600

    camps API.

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository repository;
+        private readonly IMapper mapper;
+
+        public CampsController(ICampRepository repository, IMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CampModel[]>> Get(bool includeTalks = false)
+        {
+            try
+            {
+                var results = await this.repository.GetAllCampsAsync(includeTalks);
+                return this.mapper.Map<CampModel[]>(results);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+        }
+
+        [HttpGet("{moniker}")]
+        public async Task<ActionResult<CampModel>> Get(string moniker)
+        {
+            try
+            {
+                var result = await this.repository.GetCampAsync(moniker);
+                if (result == null) return NotFound();
+                return this.mapper.Map<CampModel>(result);
+            }
+            catch
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+        }
+
+        [HttpGet("search")]
+        public async Task<ActionResult<CampModel[]>> SearchByDate(DateTime theDate, bool includeTalks = false)
+        {
+            try
+            {
+                var results = await this.repository.GetAllCampsByEventDate(theDate, includeTalks);
+                if (!results.Any()) return NotFound();
+                return this.mapper.Map<CampModel[]>(results);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+
+
+        }
+    }
+}
---
+++ src/Data/CampProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AutoMapper;
+using CoreCodeCamp.Models;
+
+namespace CoreCodeCamp.Data
+{
+    public class CampProfile : Profile
+    {
+        public CampProfile()
+        {
+            this.CreateMap<Camp, CampModel>()
+                .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName));
+        }
+    }
+}
